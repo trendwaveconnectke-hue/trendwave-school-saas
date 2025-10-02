296c4dadf2cbe13f27fd5541b0a4a0c526797bbb
TrendWave Connect <trendwaveconnectke@gmail.com>
2025-10-02 18:51:17 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SchoolLogin from './components/SchoolLogin'
+
+function App() {
+  return (
+    <div className="App">
+      <SchoolLogin />
+    </div>
+  )
+}
+
+export default App
