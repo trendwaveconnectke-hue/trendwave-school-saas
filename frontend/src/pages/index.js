ab060db07cfb319cd00de753174947df999bcb18
TrendWave Connect <trendwaveconnectke@gmail.com>
2025-10-04 10:12:30 +0100

    Create index.js

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>🎓 TrendWave Connect</h1>
+      <p>School Management Platform</p>
+      <a href="/auth/login" style={{ color: 'blue' }}>
+        Go to Admin Login
+      </a>
+    </div>
+  );
+}
